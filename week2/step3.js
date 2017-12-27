10d1ab77e0b8603762df8d41487c23e3f712ea35
anasmos <anasmos@gmail.com>
2017-12-27 22:51:51 +0100

    Update step3.js
    remove the space from the end of string in step3

--- week2/step3.js
+++ week2/step3.js
@@ -1,7 +1,7 @@
 'use strict';
 let x;
-console.log('the value of my variable x will be: undefined ');
+console.log('the value of my variable x will be: undefined');
 console.log(x);
 x = 10;
-console.log('the value of my variable x will be: 10 ');
+console.log('the value of my variable x will be: 10');
 console.log(x);
